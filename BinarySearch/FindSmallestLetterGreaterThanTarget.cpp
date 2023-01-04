0f02bba97839d4693202b4168e8a003ce7da2706
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2023-01-04 11:36:02 +0530

    Ceil problem

---
+++ BinarySearch/FindSmallestLetterGreaterThanTarget.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+     
+     int start =0;
+     int end=letters.size()-1;
+     char answer=letters[0];
+     while(start<=end)
+     {
+         int mid =start+(end-start)/2;
+
+         if(letters[mid]>target)
+         {
+             answer=letters[mid];
+             end=mid-1;
+         }
+         else{
+             start=mid+1;
+         }
+
+     }
+        return answer;
+    }
+};
