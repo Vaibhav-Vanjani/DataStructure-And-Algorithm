87aa213e80c82a4bc32bbb0e08b236c6faa3dffe
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2023-01-08 12:58:16 +0530

    New Concept

---
+++ BinarySearch/MustDoQuestion/AllNumbersWithSpecificDifference.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+  
+  long long getSum(long long mid)
+  {
+      long long ans=0;
+      while(mid)
+      {
+          ans+=mid%10;
+          mid=mid/10;
+      }
+      return ans;
+  }
+  
+  
+  bool isValid(long long mid,long long D)
+  {
+      long long sum = getSum(mid);
+      long long diff = mid-sum;
+      
+      if(diff>=D)
+      return true;
+      else
+      return false;
+  }
+  
+  
+    long long getCount(long long N , long long D) {
+        // code here
+        // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
+        // 0 0 0 0 0 00  0 0  9  9  9  9  9  9  9  9 
+        
+        if(N>=1 && N<=9 && D!=0)
+        return 0;
+        
+        
+        
+        
+        
+        long long start =0;
+        long long end = N;
+        long long ans=0;
+        while(start<=end)
+        {
+            long long mid=start+(end-start)/2;
+            if(isValid(mid,D))
+            {
+                ans+=end-mid+1;
+                end=mid-1;
+            }
+            else
+            start=mid+1;
+        }
+       
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N,D;
+
+        cin>>N>>D;
+
+        Solution ob;
+        cout << ob.getCount(N,D) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
