b31ea97b7a20499ee68ab93587747e1116826ed7
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2023-01-12 18:28:27 +0530

    Important Question

---
+++ SlidingWindow/MustDoQuestion/TakeKOfEachCharacterFromLeftAndRight.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int takeCharacters(string s, int k) {
+        
+      unordered_map<char,int> countMap;
+      
+      int n =s.size();
+      for(int i=0;i<n;i++)
+      countMap[s[i]]++;
+
+      if(countMap['a']<k or countMap['b']<k or countMap['c']<k)
+      return -1;
+     
+      int ans=INT_MAX;
+      int window=0;
+
+      int j=0;  
+      for(int i=0;i<n;i++)
+      {
+          countMap[s[i]]--;
+          window++;
+
+          while(countMap[s[i]]<k)
+          {
+              countMap[s[j]]++;
+              j++;
+
+              window--;
+          }
+
+          ans=min(ans,n-window);
+      }
+      return ans;
+
+
+
+    }
+};
