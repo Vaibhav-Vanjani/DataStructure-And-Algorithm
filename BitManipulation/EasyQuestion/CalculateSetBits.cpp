37ddb9f7a9174626c5f0e704e4150fd177ce1b26
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2023-01-16 20:15:01 +0530

    Bit Manipulation

---
+++ BitManipulation/EasyQuestion/CalculateSetBits.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        // Write Your Code here
+        int count=0; 
+        while(N)
+        {
+            if(N&1)//check if odd :: for odd No. right most bit is 1
+            {
+                count++;
+            }
+            N>>=1;//divide by 2
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
