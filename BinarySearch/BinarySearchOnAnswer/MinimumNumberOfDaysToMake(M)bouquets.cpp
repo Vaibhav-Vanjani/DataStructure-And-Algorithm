1d2ed6f0249647938e4f55ca97e4c2d7c158e8de
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2023-02-27 18:50:06 +0530

    Binary Search

--- BinarySearch/BinarySearchOnAnswer/MinimumNumberOfDaysToMake(M)bouquets.cpp
+++ BinarySearch/BinarySearchOnAnswer/MinimumNumberOfDaysToMake(M)bouquets.cpp
@@ -1,3 +1,74 @@
+revisited.
+1.
+class Solution {
+public:
+
+
+    bool isAnswer(int mid,int m,int k,vector<int> bloomday)
+    {
+        int count=0;
+        int bouquet=0;
+        for(int i=0;i<bloomday.size();i++)
+        {
+
+            if(mid>=bloomday[i])
+            {
+                count++;
+                if(count==k)
+                {
+                    bouquet++;
+                    count=0;
+
+                    if(bouquet==m)
+                    return true;
+                }
+            }
+            else
+            {
+                count=0;
+
+            }
+        }
+        return false;
+    }
+
+
+    int minDays(vector<int>& bloomday, int m, int k) {
+        // make m bouquet
+        // use k adjacent flower
+        //bloomday[i]= a flower bloom at that day can be used in exactly 1 bouquet
+
+
+    int start = INT_MAX;
+    int end = INT_MIN;
+
+         for(int i=0;i<bloomday.size();i++)
+         {
+             start=min(start,bloomday[i]);
+             end=max(end,bloomday[i]);
+         }
+
+        int res=-1;
+         while(start<=end)
+         {
+             int mid = start + (end-start)/2 ;
+             if(isAnswer(mid,m,k,bloomday))
+             {
+                 res = mid;
+                 end=mid-1;
+             }
+             else
+             {
+                 start=mid+1;
+             }
+         }
+         return res;
+    }
+};
+
+
+2.
+
 class Solution {
 public:
 
