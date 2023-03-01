46183d1254f49a71999faf185212da46c33a9925
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2023-03-01 22:38:38 +0530

    Sliding Window

--- SlidingWindow/MustDoQuestion/MaximumOfAllSubarraysOfSizeK.cpp
+++ SlidingWindow/MustDoQuestion/MaximumOfAllSubarraysOfSizeK.cpp
@@ -1,3 +1,101 @@
+revisited.
+1.
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+     
+    //  this test case will tell you why you have to pop from back and not from front
+    //  4 1 2
+    
+    // 4 1 
+    // when you will put 2 in list left side of 2 lies a minimum which is of no use
+        
+        list<int> l;
+        vector<int> ans;
+        int end =0;
+        int start=0;
+        
+        while(end<n)
+        {
+            if(l.empty())
+            {
+                l.push_back(arr[end]);
+            }
+            else
+            {
+                int x=l.back();
+                while(!l.empty() && x<arr[end])
+                {
+                    l.pop_back();
+                    x=l.back();
+                }
+                l.push_back(arr[end]);
+            }
+            
+            if((end-start+1)<k)
+            {
+                end++;
+            }
+            else if((end-start+1)==k)
+            {
+                int dummy = l.front();
+                ans.push_back(dummy);
+                
+                if(dummy==arr[start])
+                {
+                    l.pop_front();
+                }
+                
+                start++;
+                end++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, k;
+	    cin >> n >> k;
+	    
+	    int arr[n];
+	    for(int i = 0;i<n;i++) 
+	        cin >> arr[i];
+	    Solution ob;
+	    vector <int> res = ob.max_of_subarrays(arr, n, k);
+	    for (int i = 0; i < res.size (); i++) 
+	        cout << res[i] << " ";
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
+
+2.
+
 //{ Driver Code Starts
 //Initial template for C++
 
