a8e6509132ee91dd6f9970f7bf7a673366f65746
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2023-08-11 21:50:16 +0530

    Sliding Window

--- SlidingWindow/MustDoQuestion/CountOccurencesOfAnagrams.cpp
+++ SlidingWindow/MustDoQuestion/CountOccurencesOfAnagrams.cpp
@@ -10,6 +10,84 @@ class Solution{
 public:
 	int search(string pat, string txt) {
 	    // code here
+	    int size=pat.size();
+	    unordered_map<char,int> mp;
+	    
+	    for(int i=0;i<size;i++)
+	    {
+	        mp[pat[i]]++;
+	    }
+	    
+	    int count = mp.size();
+	    int i=0;
+	    int j=0;
+	    int n =txt.size();
+	    int ans =0;
+	    
+	    while(i<n && j<n)
+	    {
+	       if( mp.find(txt[j])!=mp.end() ) 
+	       {
+	           mp[txt[j]]--;
+	           
+	           if(mp[txt[j]]==0)
+	           count--;
+	       }
+	       
+	       if((j-i+1)<size)
+	       j++;
+	       else if((j-i+1)==size)
+	       {
+	            if(count==0)
+	            ans++;
+	            
+	            if(mp.find(txt[i])!=mp.end())
+	            {
+	                mp[txt[i]]++;
+	                if(mp[txt[i]]==1)
+	                count++;
+	            }
+	            
+	            i++;
+	            j++;
+	       }
+	    }
+	    return ans;
+	    
+	    
+	}
+
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string pat, txt;
+        cin >> txt >> pat;
+        Solution ob;
+        auto ans = ob.search(pat, txt);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
+------------------------------------------
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	int search(string pat, string txt) {
+	    // code here
 	int N=txt.size();
 	int k = pat.size();
 	int start=0;
