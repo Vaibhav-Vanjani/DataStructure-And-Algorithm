73c72d45c1076a783bc3e1bbd563b4d1a603d55b
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2023-08-14 21:38:04 +0530

    Stack

--- Stack/NStackInArray.cpp
+++ Stack/NStackInArray.cpp
@@ -3,6 +3,65 @@ class NStack
 {
 public:
     // Initialize your data structure.
+    int *arr;
+    int *top;
+    int *next;
+    int freespot;
+    NStack(int N, int S)
+    {
+        // Write your code here.
+        freespot=0;
+        arr = new int[S];
+        top = new int[N];
+        next = new int[S];
+        
+        for(int i =0;i<N;i++)
+        top[i]=-1;
+        
+        for(int i=0;i<S;i++)
+        {
+            next[i]=i+1;
+        }
+        next[S-1]=-1;
+    }
+
+    // Pushes 'X' into the Mth stack. Returns true if it gets pushed into the stack, and false otherwise.
+    bool push(int x, int m)
+    {
+        // Write your code here.
+        
+        if(freespot == -1)
+        return false;
+        
+        int i= freespot;
+        freespot = next[i];
+        arr[i]=x;
+        next[i]=top[m-1];
+        top[m-1]=i;
+
+        return true;
+    }
+
+    // Pops top element from Mth Stack. Returns -1 if the stack is empty, otherwise returns the popped element.
+    int pop(int m)
+    {
+        // Write your code here.
+        if(top[m-1]==-1)
+        return -1;
+        
+        int i = top[m-1];
+        top[m-1] = next[i];
+        next[i]=freespot;
+        freespot = i;
+        return arr[i];
+    }
+};
+----------------------------
+#include <bits/stdc++.h> 
+class NStack
+{
+public:
+    // Initialize your data structure.
         int free;
         int *arr;
         int *top;
