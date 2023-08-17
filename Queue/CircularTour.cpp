596dc691816dc63aacd545061daa7e21626e83fa
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2023-08-17 21:46:51 +0530

    Queue

--- Queue/CircularTour.cpp
+++ Queue/CircularTour.cpp
@@ -22,6 +22,93 @@ struct petrolPump
 class Solution{
   public:
   
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(petrolPump p[],int n)
+    {
+       //Your code here
+       vector<pair<int,int>> q(n,{0,0});
+       int sum=0;
+       int temp ;
+       int prefixsum=0;
+       int j=0;
+       
+       for(int i=0;i<n;i++)
+       {
+           temp = p[i].petrol-p[i].distance;
+           q[i] = {temp,prefixsum};
+           prefixsum+=temp;
+       }
+       
+       for(int i=0;i<n;i++)
+       {
+           sum+=q[i].first;
+           
+           if(sum<0)
+           {
+               sum=0;
+               j=i+1;
+           }
+       }
+       
+       if(j==n)return -1;
+       
+       sum+=q[j].second;
+       return sum>=0?j:-1;
+       
+       
+       
+       
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        petrolPump p[n];
+        for(int i=0;i<n;i++)
+            cin>>p[i].petrol>>p[i].distance;
+        Solution obj;
+        cout<<obj.tour(p,n)<<endl;
+    }
+}
+
+// } Driver Code Ends
+----------------
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};
+
+
+// } Driver Code Ends
+/*
+The structure of petrolPump is 
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};*/
+
+/*You are required to complete this method*/
+class Solution{
+  public:
+  
  
     bool helper(int curr,int i,int n,int arr[])
     {
